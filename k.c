a2cc439aeb5c6ad5ca6b942dc5ecdf711e36ffe2
George Crabtree <supercrabtree@googlemail.com>
2015-06-27 09:51:45 +0200

    Add print file last accessed time

--- k.c
+++ k.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
-#include <sys/stat.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
   DIR *d;
   struct dirent *dir;
+  struct stat buf;
+  int numfiles;
+  int exists;
+
   d = opendir(".");
-  if (d)
-  {
-    while ((dir = readdir(d)) != NULL)
-    {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
+  if (d == NULL) {
+    perror("prsize");
+    return 1;
   }
+
+  numfiles = 0;
+
+  while ((dir = readdir(d)) != NULL)
+  {
+    exists = stat(dir->d_name, &buf);
+    if (exists < 0) {
+      fprintf(stderr, "Couldn't find %s\n", dir->d_name);
+    } else {
+      printf("%ld %s\n", buf.st_atime, dir->d_name);
+      numfiles += buf.st_size;
+    }
+  }
+
+  closedir(d);
 
   return 0;
 }
