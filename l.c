a596b019e976ca7cd7633ecd3652d53911e71713
George Crabtree <supercrabtree@googlemail.com>
2016-09-27 10:34:13 +0200

    Abstract reading a directory into own function

--- l.c
+++ l.c
@@ -37,41 +37,78 @@ char *format_symlink (const char *filename)
   return symlink;
 }
 
+char collect_dir_contents (const char *path, char ***contents)
+{
+  size_t count = 0;
+  DIR *directory;
+  struct dirent *current_directory_item = NULL;
+
+  directory = opendir(path);
+
+  if (directory == NULL) {
+    perror("prsize");
+    return 1;
+  }
+
+
+  /* Find out how many items are in the directory */
+  while ((current_directory_item = readdir(directory)) != NULL)
+  {
+    count++;
+  }
+
+
+  /* Reset the pointer to the directory */
+  rewinddir(directory);
+
+
+  /* Allocate the right ammount of space to the contents array */
+  *contents = NULL;
+  *contents = calloc(count, sizeof(char *));
+
+
+  /* Loop through the directory again adding each item to the contents array */
+  count = 0;
+  while ((current_directory_item = readdir(directory)) != NULL)
+  {
+    (*contents)[count++] = strdup(current_directory_item->d_name);
+  }
+
+  closedir(directory);
+
+  return count;
+}
+
 
 /* Main --------------------------------------------------------------------- */
 
 int main (int argc, char *argv[])
 {
-  DIR *d;
-  struct dirent *dir;
   struct stat buf;
+  char **dir_contents;
   int exists;
+  int dir_contents_length;
+  int i;
 
-  d = opendir(".");
-  if (d == NULL) {
-    perror("prsize");
-    return 1;
-  }
+  dir_contents_length = collect_dir_contents(".", &dir_contents);
 
   printf("\n");
-  while ((dir = readdir(d)) != NULL)
-  {
-    exists = lstat(dir->d_name, &buf);
-    if (exists < 0) {
-      fprintf(stderr, "Couldn't find %s\n", dir->d_name);
-    } else {
 
+  for (i = 0; i < dir_contents_length; i++)
+  {
+    exists = lstat(dir_contents[i], &buf);
+    if (exists < 0) {
+      fprintf(stderr, "Couldn't find %s\n", dir_contents[i]);
+    } else {
       char *date_output = format_date(buf.st_mtime);
-      char *symlink_output = (S_ISLNK(buf.st_mode)) ? format_symlink(dir->d_name) : "";
+      char *symlink_output = (S_ISLNK(buf.st_mode)) ? format_symlink(dir_contents[i]) : "";
 
       printf("\033[38;5;235m%s\033[0m  %s %s\n", \
         date_output, \
-        dir->d_name, \
+        dir_contents[i], \
         symlink_output);
     }
   }
-
-  closedir(d);
 
   return 0;
 }
