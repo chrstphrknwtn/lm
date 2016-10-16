06533aa7c1d4dab9d13a545952360ba4b4291d99
Christopher Newton <christopher.k.newton@me.com>
2016-10-16 18:55:39 +0200

    Fix symlink paths

--- lm.c
+++ lm.c
@@ -4,24 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <unistd.h>
 
 /** Helpers
 ----------------------------------------------------------------------------- */
-
-/** Format symlink with arrow and path
------------------------------------------------ */
-char *format_symlink (const char *filename, const mode_t st_mode)
-{
-	static char symlink_str[PATH_MAX];
-
-	if (S_ISLNK(st_mode)) {
-		char *symlink_path;
-		symlink_path = realpath(filename, NULL);
-		sprintf(symlink_str, "→ \033[38;5;236m%s\033[0m", symlink_path);
-	}
-
-	return symlink_str;
-}
 
 /** Format short mode
 ----------------------------------------------- */
@@ -63,6 +49,31 @@ char *format_filename (const char *filename, const mode_t st_mode)
 	}
 
 	return filename_str;
+}
+
+/** Format symlink with arrow and path
+----------------------------------------------- */
+char *format_symlink (const char *filename, const mode_t st_mode, const size_t st_size)
+{
+	static char symlink_str[PATH_MAX];
+
+	if (S_ISLNK(st_mode)) {
+		char *symlink_path;
+		ssize_t r;
+
+		symlink_path = malloc(st_size + 1);
+
+		r = readlink(filename, symlink_path, st_size + 1);
+
+		if (r < 0) {
+        perror("readlink");
+        exit(EXIT_FAILURE);
+    }
+
+		symlink_path[st_size] = '\0';
+		sprintf(symlink_str, "→ \033[38;5;236m%s\033[0m", symlink_path);
+	}
+	return symlink_str;
 }
 
 /** Readdir
@@ -129,7 +140,7 @@ int main (int argc, char *argv[])
 			printf("%s %s %s\n", \
 			       format_shortmode(buf.st_mode), \
 			       format_filename(filename, buf.st_mode), \
-			       format_symlink(filename, buf.st_mode));
+			       format_symlink(filename, buf.st_mode, buf.st_size));
 		}
 	}
 
